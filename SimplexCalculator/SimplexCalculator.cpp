0e3336080b7628046529439b084b168fa8a1ec55
Noah Ratzburg <noah.ratzburg@yahoo.com>
2021-11-17 14:12:52 -0600

    Initial loop to define the tableau

--- SimplexCalculator/SimplexCalculator.cpp
+++ SimplexCalculator/SimplexCalculator.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
+using std::cin; using std::cout; using std::endl;
+
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cin.get();
+    int running = 1;
+    
+    int arr_w = -1;
+    int arr_h = -1;
+    int num_vars = -1;
+
+    while ( arr_w < 0 && arr_h < 0 && num_vars < 0 )
+    {
+        cout << "Enter number of rows in initial tableau: ";
+        cin >> arr_h;
+        cout << "Enter number of columns in initial tableau: ";
+        cin >> arr_w;
+        cout << "Enter number of variables: ";
+        cin >> num_vars;
+
+        if (arr_h < 0) cout << "~# NUMBER OF ROWS MUST BE POSITIVE #~" << endl;
+        if (arr_w < 0) cout << "~# NUMBER OF COLUMNS MUST BE POSITIVE #~" << endl;
+        if (num_vars < 0) cout << "~# NUMBER OF VARIABLES MUST BE POSITIVE #~" << endl;
+    }
+
+    cout << "Creating initial tableau | Rows " << arr_h << " | Columns " << arr_h << " | Variables " << num_vars << endl;
+
     return 1;
 }
